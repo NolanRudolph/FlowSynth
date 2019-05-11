b871e0a5f79d870d8216e8768c9750f989cec427
Nolan Rudolph <nolan@localhost.localdomain>
2019-05-11 15:19:22 -0700

    Risky buisness

--- Legit/main.c
+++ Legit/main.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 #include <sys/types.h>
 #include <sys/socket.h>
+
 #include <netdb.h>
+
 #include <netinet/in.h>
 #include <netinet/in_systm.h>
 #include <netinet/ip.h>
 #include <netinet/ip_icmp.h>
-#include <string.h>
+#include <netinet/udp.h>
+
 #include <arpa/inet.h>
 #include <sys/time.h>
 
 void create_socket(int* sock, int on);
-void configure_IP(struct ip *ip);
+void configure_IP(struct ip *ip, uint8_t proto);
 void configure_ICMP(struct icmp *icmp);
+void configure_UDP(struct udphdr *udp);
 void send_packets(int sock, struct ip *ip, struct icmp *icmp, struct sockaddr_in sendto_addr);
 
 int main(int argc, char* argv[]) {
@@ -34,31 +40,45 @@ int main(int argc, char* argv[]) {
 	sendto_addr.sin_family = AF_INET;
 	sendto_addr.sin_port = htons(9001);
 	sendto_addr.sin_addr.s_addr = inet_addr(address);
-
+	
+		/******** ICMP ********/
 
 	/* Constructing IP Struct */
-	struct ip *ip = NULL;
-	ip = (struct ip *)malloc(sizeof(struct ip));		
+	struct ip *ipICMP = NULL;
+	ipICMP = (struct ip *)malloc(sizeof(struct ip));		
 	
 	// Memory Error Handling
-	if (ip == NULL) {
-		fprintf(stderr, "Malloc error for IP struct.\n");
+	if (ipICMP == NULL) {
+		fprintf(stderr, "Malloc error for ICMP'S IP struct.\n");
+		exit(EXIT_FAILURE);
 	}
 
-	configure_IP(ip);
+	configure_IP(ipICMP, IPPROTO_ICMP);
 
 	
 	/* Constructing ICMP Struct */
-	struct icmp *icmp = (struct icmp *)(ip + 1);
-	
+	struct icmp *icmp = (struct icmp *)(ipICMP + 1);	
 	configure_ICMP(icmp);
 
 
+		/********* UDP *********/
+	
+	struct ip *ipUDP = NULL;
+	ipUDP = (struct ip *)malloc(sizeof(struct ip));
+
+	if (ipUDP == NULL) {
+		fprintf(stderr, "Malloc error for UDP'S IP struct.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	configure_IP(ipUDP, IPPROTO_UDP);
+
+	struct udphdr *udp = (struct udphdr *)(ipUDP + 1);
+	configure_UDP(udp);
+
 	/* Transmitting Packets */
 	int i;
-	for (i = 0; i < 10; ++i) {
-		send_packets(sock, ip, icmp, sendto_addr);
-	}
+	send_packets(sock, ipICMP, icmp, sendto_addr);
 
 	return 0;
 
@@ -76,12 +96,12 @@ void create_socket(int* sock, int on) {
 	}
 }
 
-void configure_IP(struct ip *ip) {
+void configure_IP(struct ip *ip, uint8_t proto) {
 
 	ip -> ip_v = 4;
 	ip -> ip_hl = 5;
 	ip -> ip_tos = 0;
-	ip -> ip_p = IPPROTO_ICMP;
+	ip -> ip_p = proto;
 	ip -> ip_ttl = 255;
 
 	// Temporary Testing on Localhost
@@ -94,9 +114,18 @@ void configure_ICMP(struct icmp *icmp) {
 
 	icmp -> icmp_type = ICMP_ECHO;
 	icmp -> icmp_code = 0;
-	icmp -> icmp_id = 123;
+	// Add icmp_cksum
 
 }
+
+void configure_UDP(struct udphdr *udp) {
+	
+	udp -> uh_sport = 123;
+	udp -> uh_dport = 123;
+	// Add uh_ulen & uh_sum
+
+}
+
 
 void send_packets(int sock, struct ip *ip, struct icmp *icmp, struct sockaddr_in sendto_addr) {	
 
