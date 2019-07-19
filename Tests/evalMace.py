4a2d3613fd8263e6e22a7d6db061d2e336035e50
Nolan Rudolph <nolan@localhost.localdomain>
2019-07-19 01:04:26 -0700

    Reduced print statements from C and Python files.

--- Tests/evalMace.py
+++ Tests/evalMace.py
@@ -41,11 +41,11 @@ def main():
 	print("Expected Packet Rate: {} | Actual Packet Rate: {}"\
 		  .format(packet_rate, str(pack_rate_av)))
 	print("Difference: {} | Accuracy: {}%".format\
-		(round(packet_rate - pack_rate_av, 2), round((pack_rate_av / packet_rate) * 100,2)))
+		(packet_rate - pack_rate_av, (pack_rate_av / packet_rate) * 100))
 	print("Expected Bit Rate: {} | Actual Bit Rate: {}"\
 		  .format(bit_rate, bit_rate_av))
 	print("Difference: {} | Accuracy: {0:.2f}%".format\
-		(round(bit_rate - bit_rate_av, 2), round((bit_rate_av / bit_rate) * 100, 2)))
+		(bit_rate - bit_rate_av, (bit_rate_av / bit_rate) * 100))
 
 if __name__ == "__main__":
 	main()
